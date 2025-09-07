ef47533c80ba6a2d0d6a18ca8da2364be15d359e
Tousif-Ahmad <tousifahmdkhan2002@gmail.com>
2025-09-07 22:58:11 +0500

    setTimeout and setInterval

---
+++ time-and-intervals/script.js
@@ -0,0 +1,32 @@
+// setTimeout and setInterval
+
+// SETTIMEOUT
+// setTimeout(function(){
+//     console.log("Heyy");
+// },1000)
+
+// let tm = setTimeout(function(){
+//     console.log("clear timeout");
+// },1000)
+// clearTimeout(tm)
+// CLEAR INTERVAL
+// setInterval(function(){
+//     console.log("har ek second baar baar chalega");
+// },1000)
+
+// let ci = clearInterval(function(){
+//     console.log("clear interval");
+// })
+// clearInterval(ci)
+
+// let count = 10;
+// let tm = setInterval(function () {
+//   if (count >= 1) {
+//     console.log(count);
+//     count--;
+    
+//   }
+//   else{
+//     clearInterval(tm)
+//   }
+// }, 1000);
